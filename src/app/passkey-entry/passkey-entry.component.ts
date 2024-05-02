82f7dba6dbd4a06ebcf12f4dc3591211fb1e0863
Nathan Wash <nwash57@gmail.com>
2024-05-02 00:40:10 -0500

    make passkey entry pretty

--- src/app/passkey-entry/passkey-entry.component.ts
+++ src/app/passkey-entry/passkey-entry.component.ts
@@ -1,27 +1,36 @@
 import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { FunctionCodeService } from "../function-code.service";
+import { NgIf } from "@angular/common";
+import { delay } from "rxjs";
 
 @Component({
   selector: 'app-passkey-entry',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './passkey-entry.component.html',
   styleUrl: './passkey-entry.component.scss',
-  host: { class: 'flex' }
+  host: {class: 'flex flex-col h-full justify-center'}
 })
 export class PasskeyEntryComponent {
   private readonly functionCodeService = inject(FunctionCodeService);
 
   @Output() functionCodeChange = new EventEmitter<string>();
 
+  isLoading: boolean = false;
   passKey: string | null = null;
 
   onSubmitPasskey() {
     if (this.passKey != null) {
-      this.functionCodeService.fetchFunctionCode(this.passKey).subscribe(code => {
-        this.functionCodeChange.emit(code);
-      });
+      this.isLoading = true;
+
+      this.functionCodeService.fetchFunctionCode(this.passKey)
+        .pipe(
+          delay(2000))
+        .subscribe(code => {
+          this.functionCodeChange.emit(code);
+          this.isLoading = false;
+        });
     }
   }
 }
